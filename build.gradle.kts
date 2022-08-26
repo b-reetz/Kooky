3ed344f3fab296ced7b3ca9a72aafc760bad3e3e
Brendan Reetz <breetz@atlassian.com>
2022-08-26 13:13:50 +1200

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-    implementation("com.android.tools.build:gradle:7.1.3")
+    implementation("com.android.tools.build:gradle:7.2.2")
 }
